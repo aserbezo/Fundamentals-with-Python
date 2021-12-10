d4d71ac6be204527f716ce18a7cac8f352ddd46f
aserbezo <83031663+aserbezo@users.noreply.github.com>
2021-12-10 09:58:00 +0200

    Delete 7.  Dictionary directory

--- 7.  Dictionary/3. Capitals.py
+++
@@ -1,7 +0,0 @@
-countries = input().split(", ")
-capitals = input().split(", ")
-
-capitals_dict = dict(zip(countries, capitals))
-
-for (key, value) in capitals_dict.items():
-    print(f"{key} -> {value}")
